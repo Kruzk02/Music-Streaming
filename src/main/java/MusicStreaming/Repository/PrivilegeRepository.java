ab13f805678aad61058017f03f191c20b289c525
Thien Phuc <thienphuc484@gmail.com>
2024-01-18 13:59:06 +0700

    Add Basic Authentication Model and Repository.

---
+++ src/main/java/MusicStreaming/Repository/PrivilegeRepository.java
@@ -0,0 +1,8 @@
+package MusicStreaming.Repository;
+
+import MusicStreaming.Model.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege,Long> {
+    Privilege findByName(String name);
+}
---
+++ src/main/java/MusicStreaming/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package MusicStreaming.Repository;
+
+import MusicStreaming.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
