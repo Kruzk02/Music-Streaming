c8cc1bf350ec7928afef87ae140fa899feb40375
Thien Phuc <thienphuc484@gmail.com>
2024-01-19 09:57:17 +0700

    Implement User Basic Authentication.

---
+++ src/main/java/MusicStreaming/Config/SetupData.java
@@ -0,0 +1,70 @@
+package MusicStreaming.Config;
+
+import MusicStreaming.Model.Privilege;
+import MusicStreaming.Model.Role;
+import MusicStreaming.Model.User;
+import MusicStreaming.Repository.PrivilegeRepository;
+import MusicStreaming.Repository.RoleRepository;
+import MusicStreaming.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class SetupData implements ApplicationListener<ContextRefreshedEvent> {
+    boolean alreadySetup = false;
+
+    @Autowired private UserRepository userRepository;
+    @Autowired private RoleRepository roleRepository;
+    @Autowired private PrivilegeRepository privilegeRepository;
+    @Autowired private PasswordEncoder passwordEncoder;
+    @Override
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if(alreadySetup)return;
+        Privilege readPrivilege = createPrivilegeIfNotFound("READ_PRIVILEGE");
+        Privilege writePrivilege = createPrivilegeIfNotFound("WRITE_PRIVILEGE");
+
+        List<Privilege> adminPrivilege = Arrays.asList(readPrivilege,writePrivilege);
+        createRoleIfNotFound("ROLE_ADMIN",adminPrivilege);
+        createRoleIfNotFound("ROLE_USER",Arrays.asList(readPrivilege));
+
+//        Role adminRole = roleRepository.findByName("ROLE_ADMIN");
+//        User user = new User();
+//        user.setUsername("phucnguyen");
+//        user.setEmail("phucnguyen@gmail.com");
+//        user.setPassword(passwordEncoder.encode("123123"));
+//        user.setRoles(Arrays.asList(adminRole));
+//        userRepository.save(user);
+
+        alreadySetup = true;
+    }
+
+    @Transactional
+    private Privilege createPrivilegeIfNotFound(String name){
+        Privilege privilege = privilegeRepository.findByName(name);
+        if(privilege == null){
+            privilege = new Privilege(name);
+            privilegeRepository.save(privilege);
+        }
+        return privilege;
+    }
+
+    @Transactional
+    private Role createRoleIfNotFound(String name, Collection<Privilege> privileges){
+        Role role = roleRepository.findByName(name);
+        if(role == null){
+            role = new Role(name);
+            role.setPrivileges(privileges);
+            roleRepository.save(role);
+        }
+        return role;
+    }
+}
